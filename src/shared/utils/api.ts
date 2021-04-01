ef09b4083822f016d0b64085f12a57c659635b55
Atom <bodynar@gmail.com>
2021-04-02 00:03:50 +0300

    add api utils

---
+++ src/shared/utils/api.ts
@@ -0,0 +1,70 @@
+import { BaseResponse, BaseResponseWithResult } from "models/response/baseResponse";
+
+import { isNullOrUndefined } from "./common";
+
+type RequestData = {
+    // TODO: fix type
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [propertyName: string]: any;
+};
+
+/**
+ * Send data to api to process
+ * @param uri Api endpoint address
+ * @param requestData Request data
+ * @returns {Promise<void>} Promise with api processing result
+ */
+export const post = async (uri: string, requestData: RequestData): Promise<void> => {
+    const response: Response = await fetch(uri, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json;charset=UTF-8',
+        },
+        body: JSON.stringify(requestData)
+    });
+
+    const baseResponse: BaseResponse = await response.json()
+
+    if (response.ok) {
+        if (baseResponse.success) {
+            return Promise.resolve();
+        } else {
+            return Promise.reject(new Error(baseResponse.erorr));
+        }
+    } else {
+        return Promise.reject(new Error(response.statusText));
+    }
+};
+
+/**
+ * Gather data from specified api
+ * @param uri Api endpoint address
+ * @param requestData Request data
+ * @returns {Promise<TResult>} Promise with api get result
+ */
+export const get = async <TResult>(uri: string, requestData?: RequestData): Promise<TResult> => {
+    const requestParams: RequestInit = {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json;charset=UTF-8',
+        },
+        body: undefined
+    };
+
+    if (!isNullOrUndefined(requestData)) {
+        requestParams.body = JSON.stringify(requestData);
+    }
+
+    const response: Response = await fetch(uri, requestParams);
+    const baseResponse: BaseResponseWithResult<TResult> = await response.json();
+
+    if (response.ok) {
+        if (baseResponse.success) {
+            return Promise.resolve(baseResponse.result);
+        } else {
+            return Promise.reject(new Error(baseResponse.erorr));
+        }
+    } else {
+        return Promise.reject(new Error(response.statusText));
+    }
+};
