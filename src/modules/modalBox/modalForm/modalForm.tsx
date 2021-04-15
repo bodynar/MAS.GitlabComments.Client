0e6a493ebe21f63e0a524f4f1a3b4d464438ea80
Atom <bodynar@gmail.com>
2021-04-15 19:44:34 +0300

    partially implement modal form

--- src/modules/modalBox/modalForm/modalForm.tsx
+++ src/modules/modalBox/modalForm/modalForm.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { isNullOrUndefined } from 'utils/common';
+
+import Text from 'modules/modalBox/modalForm/components/text/text';
+
 import { ModalFormConfiguration } from './types';
 
 type ModalFormProps = {
@@ -11,5 +15,19 @@ export const ModalForm = ({ formConfig }: ModalFormProps): JSX.Element => {
         throw new Error("No field provided for ModalForm");
     }
 
-    return <>{formConfig.fields.map(x => x.name).join(", ")}</>;
+    return (
+        <div>
+            {!isNullOrUndefined(formConfig.caption)
+                && <h3>{formConfig.caption}</h3>}
+            {formConfig.fields.map(fieldConfig => {
+                if (fieldConfig.type === 'text') {
+                    return <Text
+                        key={fieldConfig.name}
+                        fieldConfig={fieldConfig}
+                    />;
+                }
+                return <>{fieldConfig.type} - {fieldConfig.name}</>;
+            })}
+        </div>
+    );
 };
