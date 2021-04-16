33bc5ac5d42b39854c531bfbf1ff9168c57b0710
Atom <bodynar@gmail.com>
2021-04-17 02:24:50 +0300

    add navbar styles, navbar menu

---
+++ src/modules/app/components/navbar/menu.ts
@@ -0,0 +1,31 @@
+import { isStringEmpty } from "utils/common";
+
+/** Model for navbar menu items */
+export interface MenuItem {
+    /** Unique name */
+    name: string;
+
+    /** Caption */
+    caption: string;
+
+    /** Target route link */
+    link: string;
+
+    /** Should item be readonly */
+    disabled?: boolean;
+}
+
+/** Static navbar menu */
+export const menuItems: Array<MenuItem> = [
+    {
+        name: 'Comments',
+        caption: 'Comments',
+        link: '/'
+    },
+    // {
+    //     name: 'stats',
+    //     caption: 'Statistics',
+    //     link: '/stats/',
+    //     disabled: true
+    // }
+].filter(x => !isStringEmpty(x.name) && !isStringEmpty(x.link));
--- src/modules/app/components/navbar/navbar.tsx
+++ src/modules/app/components/navbar/navbar.tsx
@@ -1,44 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+
+import './navbar.scss';
 
 import GitLabLogo from '../gitlabLogo/gitlabLogo';
+
+import { MenuItem, menuItems } from './menu';
 
 type NavbarProps = {
 
 };
 
 export default function Navbar({ }: NavbarProps): JSX.Element {
+    const [activeMenuItem, setActiveMenuItem] = useState<string>(menuItems[0].name);
     return (
         <nav
-            className="navbar is-link"
+            className="app-navbar navbar is-link"
             role="navigation"
             aria-label="main navigation"
         >
             <GitLabLogo />
-            <div
-                className="navbar-menu"
-            >
+            <div className="navbar-menu" >
                 <div className="navbar-start">
-                    <a className="navbar-item">
-                        Home
-                    </a>
-                    <a className="navbar-item">
-                        Documentation
-                    </a>
+                    {menuItems.map(menuItem =>
+                        <NavbarMenuItem
+                            key={menuItem.name}
+                            item={menuItem}
+                            isSingle={menuItems.length === 1}
+                            isActive={menuItem.name === activeMenuItem}
+                            onClick={setActiveMenuItem}
+                        />
+                    )}
                 </div>
-
                 <div className="navbar-end">
-                    <div className="navbar-item">
-                        <div className="buttons">
-                            <a className="button is-primary">
-                                <strong>Sign up</strong>
-                            </a>
-                            <a className="button is-light">
-                                Log in
-                            </a>
-                        </div>
-                    </div>
+
                 </div>
             </div>
         </nav>
     );
 }
+
+type NavbarMenuItemProps = {
+    item: MenuItem;
+    isSingle: boolean;
+    isActive: boolean;
+    onClick: (name: string) => void;
+};
+
+const NavbarMenuItem = ({ item, isActive, onClick, isSingle }: NavbarMenuItemProps): JSX.Element => {
+    const href: string | undefined =
+        item.disabled === true || isActive
+            ? undefined
+            : item.link;
+
+    let className = 'app-navbar__item';
+
+    if (isSingle) {
+        className += ' app-navbar__item--single';
+    } else {
+        if (item.disabled === true) {
+            className += ' app-navbar__item--disabled';
+        }
+        if (isActive) {
+            className += ' app-navbar__item--active';
+        }
+    }
+
+    const onHrefClick = React.useCallback(
+        () => {
+            if (item.disabled !== true && !isActive) {
+                onClick(item.name);
+            }
+        }, [isActive, item.disabled, item.name, onClick]);
+
+    return (
+        <a
+            className={className}
+            href={href}
+            onClick={onHrefClick}
+        >
+            {item.caption}
+        </a>
+    );
+};
