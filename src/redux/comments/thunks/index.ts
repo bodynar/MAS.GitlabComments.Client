2fe6d3f18461cd79fb2f712722295fe844d47fce
Atom <bodynar@gmail.com>
2021-09-14 00:42:24 +0300

    #6 separate comment thunks in single files

---
+++ src/redux/comments/thunks/index.ts
@@ -0,0 +1,15 @@
+import { getAllComments } from './getAllComments';
+import { addComment } from './addComment';
+import { updateComment } from './updateComment';
+import { increment } from './increment';
+import { showDescription } from './showDescription';
+import { deleteComment } from './deleteComment';
+
+export {
+    getAllComments,
+    addComment,
+    updateComment,
+    increment,
+    showDescription,
+    deleteComment
+};
