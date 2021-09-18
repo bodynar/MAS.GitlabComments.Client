dc34eeed897a948aa911d619fe3fd16f1fe102f5
Atom <bodynar@gmail.com>
2021-09-18 20:11:49 +0300

    #6 add bell component

---
+++ src/modules/app/components/bell/component/bell.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+
+import { connect } from 'react-redux';
+
+import './bell.scss';
+
+import { NotificationHistoryItem } from '@app/models/notification';
+
+import { CompositeAppState } from '@app/redux/rootReducer';
+import { setNotificationsBadgeToZero } from '@app/redux/notificator/actions';
+
+import NotificationStoryRecord from '../components/notificationStoryRecord';
+
+type BellProps = {
+    /** All notifications in current session */
+    notifications: Array<NotificationHistoryItem>;
+
+    /** Amount of unread notifications */
+    notificationBadge: number;
+
+    /** Handler of open notifications list action  */
+    onListOpened: () => void;
+};
+
+/** Bell with notifications component */
+function Bell(props: BellProps): JSX.Element {
+    const [isListVisible, setListVisibility] = React.useState<boolean>(false);
+
+    const onBellClick = React.useCallback(
+        () => {
+            setListVisibility(!isListVisible);
+
+            if (props.notificationBadge !== 0) {
+                props.onListOpened();
+            }
+        }, [isListVisible, props.notificationBadge, props.onListOpened]
+    );
+
+    const shouldBadgeBeVisible: boolean = props.notificationBadge > 0;
+    const badgeNumber: string = props.notificationBadge > 10 ? '9+' : `${props.notificationBadge}`;
+    const title: string = shouldBadgeBeVisible ? `${badgeNumber} new notifications` : 'No new notifications';
+
+    return (
+        <div className="app-bell">
+            <div
+                className={`app-bell__icon${isListVisible ? " app-bell__icon--opened" : ""}`}
+                onClick={onBellClick}
+                title={title}
+            >
+                <span className="icon">
+                    <i className="gg-bell"></i>
+                </span>
+                {shouldBadgeBeVisible &&
+                    <span className="app-bell__badge">{badgeNumber}</span>
+                }
+            </div>
+            <div className="app-bell__list" aria-hidden={!isListVisible}>
+                <BellList notifications={props.notifications} />
+            </div>
+        </div>
+    );
+}
+
+/** Bell notification list component */
+function BellList({ notifications }: { notifications: Array<NotificationHistoryItem>; }): JSX.Element {
+    if (notifications.length > 0) {
+        return (
+            <ul>
+                {notifications.map(x =>
+                    <NotificationStoryRecord
+                        key={x.id}
+                        item={x}
+                    />
+                )}
+            </ul>
+        );
+    }
+
+    return (
+        <span className="app-bell__empty-list">
+            You haven&apos;t received any notifications yet
+        </span>
+    );
+}
+
+export default connect(
+    ({ notificator }: CompositeAppState) => ({
+        notifications: notificator.history,
+        notificationBadge: notificator.historyBadgeCount
+    }),
+    { onListOpened: setNotificationsBadgeToZero }
+)(Bell);
