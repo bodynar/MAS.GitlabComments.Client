2e16158de9dce59b0d31832a8e50ac29ed98a888
bodynar <bodynar@gmail.com>
2022-04-16 02:09:56 +0300

    #28 update navbar brand component

--- src/modules/app/components/navbar/components/navbarBrand/navbar-brand.tsx
+++ src/modules/app/components/navbar/components/navbarBrand/navbar-brand.tsx
@@ -7,16 +7,11 @@ import { GitlabLogoIcon } from '../';
 /** Navbar app brand */
 export default function NavbarBrand(): JSX.Element {
     return (
-        <div className="navbar-brand">
-            <div className="app-navbar__brand">
-                <a className="app-navbar__logo">
-                    <GitlabLogoIcon />
-                    <span className="app-navbar__name is-unselectable">
-                        Gitlab Comments
-                    </span>
-                </a>
-            </div>
-
+        <div className="navbar-brand app-navbar__brand">
+            <GitlabLogoIcon />
+            <span className="app-navbar__name is-unselectable">
+                Gitlab Comments
+            </span>
         </div>
     );
 }
