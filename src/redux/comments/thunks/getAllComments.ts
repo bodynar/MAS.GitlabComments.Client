551c952271d9907360b7bb1c45cbd7afb4b4baab
bodynar <bodynar@gmail.com>
2022-04-22 02:28:46 +0300

    #28 mock comments

--- src/redux/comments/thunks/getAllComments.ts
+++ src/redux/comments/thunks/getAllComments.ts
@@ -1,12 +1,15 @@
 import { ThunkAction, ThunkDispatch } from "redux-thunk";
 
-import { get } from "@app/utils/api";
+// import { get } from "@app/utils/api";
 
 import { CompositeAppState } from "@app/redux/rootReducer";
 import { ActionWithPayload } from "@app/redux/types";
 
+import { Comment } from "@app/models/comment";
+
 import { setComments } from "../actions";
-import { getSetIsLoadingAction, setError } from "../utils";
+import { getSetIsLoadingAction } from "../utils";
+import { generateGuid } from "@app/utils/guid";
 
 /**
  * Get all comments from api
@@ -18,16 +21,34 @@ export const getAllComments = (): ThunkAction<void, CompositeAppState, unknown, 
     ): void => {
         dispatch(getSetIsLoadingAction(true));
 
-        get<Array<Comment>>(`/api/comments/getAll`)
-            .then((comments: Array<Comment>) => {
-                dispatch({
-                    type: setComments,
-                    payload: {
-                        comments: comments
-                    }
-                });
+        getState();
+        // get<Array<Comment>>(`/api/comments/getAll`)
+        // .then((comments: Array<Comment>) => {
+        dispatch({
+            type: setComments,
+            payload: {
+                comments: mockComments
+            }
+        });
 
-                dispatch(getSetIsLoadingAction(false));
-            })
-            .catch(setError(dispatch, getState));
+        dispatch(getSetIsLoadingAction(false));
+        // })
+        // .catch(setError(dispatch, getState));
     };
+
+const mockComments: Array<Comment> =
+    new Array(100)
+        .fill(0, 0, 100)
+        .map((_, i) => ({
+            id: generateGuid(),
+            appearanceCount: i + 1,
+            message: `Comment ${i + 1}`
+        }))
+        .map(x => ({
+            ...x,
+            id: x.appearanceCount === 15 ? 'afb494f8-5977-4a12-9fca-b7da599802e4' : x.id
+        }))
+        .map(x => ({
+            ...x,
+            message: `Comment ${x.id}`
+        }));
