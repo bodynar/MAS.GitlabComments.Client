c644baaa1a58cd7719de2a611376ec506c353c89
bodynar <bodynar@gmail.com>
2022-04-24 19:37:53 +0300

    #28 fix ref

--- src/redux/stats/reducer.ts
+++ src/redux/stats/reducer.ts
@@ -2,7 +2,8 @@ import { StatsRecord } from "@app/models/response/statsRecord";
 import { getPropertyValueWithCheck } from "@app/utils/object";
 
 import { ActionWithPayload } from "../types";
-import { SetStatsData, SetStatsFilters, StatsFilter, StatsState } from "./types";
+import { SetStatsData, SetStatsFilters } from "./actions";
+import { StatsFilter, StatsState } from "./types";
 
 /** Default stats module state */
 const defaultState: StatsState = {
