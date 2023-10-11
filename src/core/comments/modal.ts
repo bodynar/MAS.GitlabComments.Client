ef1c9faa0a4667cba9bab5e510a5bef20745cbc5
bodynar <bodynar@gmail.com>
2023-10-11 20:36:14 +0300

    extract comment modal configs into core file

---
+++ src/core/comments/modal.ts
@@ -0,0 +1,100 @@
+import { isNullOrUndefined } from "@bodynarf/utils";
+
+import { EditCommentModel } from "@app/models/comments";
+import { ModalFormItem, ModalFormItemType, ModalParams, ModalType } from "@app/models/modal";
+
+/**
+ * List of required comment entity fields
+ * @constant
+*/
+const requiredFields = ["message", "commentWithLinkToRule"];
+
+/**
+ * Map column name to form field config
+ * @constant
+*/
+const modalFormConfig: Map<string, ModalFormItem> =
+    new Map([
+        [
+            "message",
+            {
+                name: "message",
+                type: ModalFormItemType.Text,
+                caption: "Comment",
+            }
+        ],
+        [
+            "commentWithLinkToRule",
+            {
+                name: "commentWithLinkToRule",
+                type: ModalFormItemType.Text,
+                caption: "Link to rule",
+            },
+        ],
+        [
+            "description",
+            {
+                name: "description",
+                type: ModalFormItemType.Multiline,
+                caption: "Description",
+            },
+        ]
+    ]);
+
+/**
+ * Get modal configuration for edit comment (create or edit mode)
+ * @param comment Comment data (for edit modal)
+ * @returns Modal configuration parameters
+ */
+export const getEditModalConfig = (
+    comment: EditCommentModel | undefined = undefined
+): ModalParams => {
+    const isCommentModelDefined =
+        !isNullOrUndefined(comment);
+
+    const formConfig: Array<ModalFormItem> = [];
+
+    modalFormConfig.forEach((config, key) => {
+        const isRequired = requiredFields.includes(key);
+
+        formConfig.push({
+            ...config,
+            isRequired,
+            value: isCommentModelDefined
+                ? comment![key as keyof EditCommentModel]
+                : "",
+        });
+    });
+
+    return {
+        modalType: ModalType.Form,
+        title: isCommentModelDefined ? "Update comment" : "Add comment",
+        formData: {
+            fields: formConfig,
+            readonly: false,
+        },
+    };
+};
+
+/**
+ * Get modal config for view comment data
+ * @returns Modal configuration parameters
+ */
+export const getViewModalConfig = (comment: EditCommentModel): ModalParams => {
+    return {
+        modalType: ModalType.Form,
+        title: "Comment info",
+        formData: {
+            readonly: true,
+            fields:
+                Array
+                    .from(modalFormConfig.values())
+                    .map(config => ({
+                        ...config,
+                        value: comment[config.name as keyof EditCommentModel]
+                    })),
+        },
+    };
+};
+
+
--- src/redux/comments/utils.ts
+++ src/redux/comments/utils.ts
@@ -4,47 +4,10 @@ import { ThunkAction, ThunkDispatch } from "redux-thunk";
 import { isNullOrUndefined } from "@bodynarf/utils";
 
 import { EditCommentModel } from "@app/models/comments";
-import { ModalFormItem, ModalFormItemType } from "@app/models/modal";
 
 import { CompositeAppState } from "@app/redux";
 
-import { ModalCallback, ModalCloseData, ModalParams, ModalType } from "@app/models/modal";
-import { setIsLoadingState } from "@app/redux/app";
-
-/**
- * Get comment form configuration for form in modal box
- * @param commentShortModel Comment data model, optional
- * @returns Modal params configuration for modal with comment form
- */
-export const getCommentModalFormConfig = (commentShortModel?: EditCommentModel): ModalParams => {
-    const isCommentModelDefined =
-        !isNullOrUndefined(commentShortModel);
-
-    const modalFields: Array<ModalFormItem> =
-        [
-            {
-                name: "Comment",
-                type: ModalFormItemType.Text,
-                caption: "Comment",
-                isRequired: true,
-                value: isCommentModelDefined ? commentShortModel?.message as string : ""
-            },
-            {
-                name: "Description",
-                type: ModalFormItemType.Multiline,
-                caption: "Description",
-                value: isCommentModelDefined ? commentShortModel?.description as string : ""
-            },
-        ];
-
-    const modalParams: ModalParams = {
-        modalType: ModalType.Form,
-        title: isCommentModelDefined ? "Update comment" : "Add comment",
-        formData: { fields: modalFields },
-    };
-
-    return modalParams;
-};
+import { ModalCallback, ModalCloseData } from "@app/models/modal";
 
 /**
  * Get modal callback configuration with custom callback
@@ -58,8 +21,6 @@ export const getCommentModalFormCallbackConfig = (
 ): ModalCallback => {
     return {
         saveCallback: (modalData: ModalCloseData): void => {
-            dispatch(setIsLoadingState(false));
-
             const message: string | undefined = modalData.formData?.fields.find(x => x.name === "Comment")?.value;
 
             if (isNullOrUndefined(message)) {
@@ -68,13 +29,11 @@ export const getCommentModalFormCallbackConfig = (
 
             const comment: EditCommentModel = {
                 message: message as string,
-                description: modalData.formData?.fields.find(x => x.name === "Description")?.value
+                description: modalData.formData?.fields.find(x => x.name === "Description")?.value,
+                commentWithLinkToRule: modalData.formData?.fields.find(x => x.name === "CommentWithLinkToRule")?.value!,
             };
 
             dispatch(action(comment));
-        },
-        cancelCallback: (): void => {
-            dispatch(setIsLoadingState(false));
         },
     };
 };
