61e7cfeb8a2f1180aba22c30ee4eb355776e6f83
bodynar <bodynar@gmail.com>
2023-11-02 00:28:40 +0300

    add user module ref

---
+++ src/modules/app/components/navbar/components/user/index.tsx
@@ -0,0 +1,46 @@
+import { useCallback } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import { getClassName } from "@bodynarf/utils";
+import { ElementSize } from "@bodynarf/react.components";
+import Icon from "@bodynarf/react.components/components/icon/component";
+
+import "./style.scss";
+
+/** User module ref icon component */
+const UserModuleRef = (): JSX.Element => {
+    const navigate = useNavigate();
+    const { pathname } = useLocation();
+
+    const isUserModuleOpened = pathname.startsWith("/user");
+
+    const onIconClick = useCallback(() => {
+
+        if (isUserModuleOpened) {
+            return;
+        }
+
+        navigate("/user/", { replace: true });
+    }, [isUserModuleOpened, navigate]);
+
+    const className = getClassName([
+        "app-navbar__item",
+        "app-user-ref",
+        isUserModuleOpened ? "app-user-ref--active" : ""
+    ]);
+
+    return (
+        <div
+            className={className}
+            onClick={onIconClick}
+        >
+            <Icon
+                name={isUserModuleOpened ? "gear-fill" : "gear"}
+                size={ElementSize.Medium}
+            />
+        </div>
+    );
+};
+
+/** User module ref icon component */
+export default UserModuleRef;
