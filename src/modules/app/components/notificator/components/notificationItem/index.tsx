2bae9d46767fc2613fa360c98b81436f1e0b439b
bodynar <bodynar@gmail.com>
2023-11-07 21:46:49 +0300

    add todo mark

--- src/modules/app/components/notificator/components/notificationItem/index.tsx
+++ src/modules/app/components/notificator/components/notificationItem/index.tsx
@@ -35,6 +35,7 @@ export default function Notification({
     useEffect(() => {
         if (!item.important) {
             const timer = setTimeout(hide, NOTIFICATION_HIDE_DELAY);
+            // TODO: Do not hide when mouse hover. Hide after mouse leave + 0.5sec
 
             return (): void => { clearTimeout(timer); };
         }
